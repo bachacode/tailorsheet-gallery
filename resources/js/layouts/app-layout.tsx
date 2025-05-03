1bebafce62b8371e71c8414a5311d127b33e3450
bachacode <85910843+bachacode@users.noreply.github.com>
2025-05-03 17:49:48 -0400

    feat: enhance flash message handling in AppLayout with toast notifications

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -1,16 +1,56 @@
 import { Toaster } from '@/components/ui/sonner';
 import AppLayoutTemplate from '@/layouts/app/app-header-layout';
 import { type BreadcrumbItem } from '@/types';
-import { type ReactNode } from 'react';
+import { usePage } from '@inertiajs/react';
+import { useEffect, useRef, type ReactNode } from 'react';
+import { toast } from 'sonner';
 
 interface AppLayoutProps {
-    children: ReactNode;
-    breadcrumbs?: BreadcrumbItem[];
+  children: ReactNode;
+  breadcrumbs?: BreadcrumbItem[];
 }
 
-export default ({ children, breadcrumbs, ...props }: AppLayoutProps) => (
+interface FlashProps {
+  success?: string;
+  error?: string;
+  [key: string]: string | undefined
+}
+
+interface PageProps {
+  flash: FlashProps
+  [key: string]: unknown
+}
+
+export default function ({ children, breadcrumbs, ...props }: AppLayoutProps) {
+  const { flash } = usePage<PageProps>().props
+  const prevFlashRef = useRef<FlashProps>({})
+
+  useEffect(() => {
+    const prevFlash = prevFlashRef.current
+    console.log(flash && flash.success !== prevFlash.success);
+    if (flash) {
+      if (flash.success && flash.success !== prevFlash.success) {
+        toast.success(flash.success, {
+          closeButton: true,
+          duration: 3000,
+          position: 'top-right',
+        });
+      }
+
+      if (flash.error && flash.error !== prevFlash.error) {
+        toast.error(flash.error, {
+          closeButton: true,
+          duration: 3000,
+          position: 'top-right',
+        });
+      }
+    }
+    prevFlashRef.current = flash
+  }, [flash]);
+  return (
     <AppLayoutTemplate breadcrumbs={breadcrumbs} {...props}>
-        {children}
-        <Toaster/>
+      {children}
+      <Toaster />
     </AppLayoutTemplate>
-);
+  )
+};
