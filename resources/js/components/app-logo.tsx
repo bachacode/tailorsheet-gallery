54a7c8db84c7b346559f8e327bc6d36b5162d5e5
bachacode <85910843+bachacode@users.noreply.github.com>
2025-05-02 22:36:34 -0400

    Update app logo icon and change logo text to "Galería TailorSheet"
    - Replaced the SVG logo in AppLogoIcon component with a new design.
    - Updated the displayed text in AppLogo component from "Laravel Starter Kit" to "Galería TailorSheet".

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,5 +1,4 @@
 import AppLogoIcon from './app-logo-icon';
-
 export default function AppLogo() {
     return (
         <>
@@ -7,7 +6,7 @@ export default function AppLogo() {
                 <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
+                <span className="mb-0.5 truncate leading-none font-semibold">Galería TailorSheet</span>
             </div>
         </>
     );
