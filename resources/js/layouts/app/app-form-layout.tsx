6fd57a0a94b22e52cc0165157f7375699de3c39c
bachacode <85910843+bachacode@users.noreply.github.com>
2025-05-05 21:19:17 -0400

    feat: add progress indicator to submit button in AppFormLayout component

--- resources/js/layouts/app/app-form-layout.tsx
+++ resources/js/layouts/app/app-form-layout.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Link } from "@inertiajs/react";
 import { LoaderCircle, LucideMoveLeft } from "lucide-react";
 import React from "react";
+import { AxiosProgressEvent } from 'axios';
 
 interface AppFormLayout {
   headerTitle: string;
@@ -13,6 +14,7 @@ interface AppFormLayout {
   submitText: string;
   processing: boolean;
   onProcessText: string;
+  progress?: AxiosProgressEvent | null
 }
 
 export default function AppFormLayout({
@@ -23,7 +25,8 @@ export default function AppFormLayout({
   onSubmit,
   processing,
   submitText,
-  onProcessText
+  onProcessText,
+  progress
 }: AppFormLayout) {
   return (
     <div className="py-8 px-12 space-y-6 border rounded-xl shadow-sm gap-6 flex flex-col mt-10">
@@ -49,6 +52,11 @@ export default function AppFormLayout({
           <Button type="submit" disabled={processing} className="bg-blue-500 hover:bg-blue-400 min-w-xs transition-colors text-white px-4 py-6 rounded cursor-pointer">
             {processing && <LoaderCircle className="h-4 w-4 animate-spin" />}
             {processing ? onProcessText : submitText}
+            {progress && (
+              <progress value={progress.percentage} max="100">
+                {progress.percentage}%
+              </progress>
+            )}
           </Button>
         </div>
         </form>
