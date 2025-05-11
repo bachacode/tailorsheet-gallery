057c73f6b031aa33a44009a9880bcff3752a9a97
bachacode <85910843+bachacode@users.noreply.github.com>
2025-05-11 16:52:52 -0400

    feat: add accordion component for image upload in album show page

--- resources/js/pages/albums/show.tsx
+++ resources/js/pages/albums/show.tsx
@@ -1,7 +1,7 @@
 import { DataTable } from '@/components/common/data-table';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
-import { Head, router, usePage, } from '@inertiajs/react';
+import { Head, router, useForm, usePage, } from '@inertiajs/react';
 import { Album } from './columns';
 import { createColumns } from '../images/columns';
 import { useState } from 'react';
@@ -10,11 +10,19 @@ import { toast } from 'sonner';
 import DataTableToggle from '@/components/common/data-table-toggle';
 import { DataGrid } from '@/components/common/data-grid';
 import AppFeatureLayout from '@/layouts/app/app-feature-layout';
-
+import FormField from '@/components/common/form-field';
+import ImageUploader from '@/components/images/image-uploader';
+import { Button } from '@/components/ui/button';
+import { LucideLoaderCircle } from 'lucide-react';
+import { Accordion,AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
 
 interface PageProps {
   album: Album;
   [x: string]: unknown;
+}
+
+interface AlbumForm {
+  images?: File[];
 }
 
 export default function Show() {
@@ -66,17 +74,100 @@ export default function Show() {
 
   const columns = createColumns(copiedState, handleCopy, handleDelete);
 
+
+  const { data, setData, post, processing, errors, setError } = useForm<Required<AlbumForm>>({
+    images: []
+  });
+
+  // const [images, setImages] = useState<File[]>([]);
+  const [isUploadingImages, setIsUploadingImages] = useState(false);
+
+  const handleAddImages = (files: File[]) => {// Clear any previous errors
+    setIsUploadingImages(true);
+    // Validate file extensions
+    const validExtensions = ["png", "jpeg", "jpg", "webp", "gif", "bmp", "tiff"];
+    const invalidFiles = files.filter(
+      (file) => !validExtensions.includes(file.name.split(".").pop()?.toLowerCase() || "")
+    );
+
+    if (invalidFiles.length > 0) {
+      setError('images', "Algunos archivos tienen extensiones no permitidas.");
+      setIsUploadingImages(false);
+      return;
+    } else {
+      setError('images', '');
+    }
+
+    // Simulate network delay
+    setTimeout(() => {
+      setData('images', files);
+      setIsUploadingImages(false);
+    }, 600);
+
+  };
+
+  const handleRemoveImage = (id: number) => {
+    setData('images', data.images.filter((_, i) => i !== id))
+    // setImages((prev) => prev.filter((image, index) => index !== id) );
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (data.images.length === 0) {
+      setError('images', "Debes subir al menos un archivo.");
+      return;
+    }
+    setIsUploadingImages(true);
+
+    post(route("albums.upload", { id: album.id }))
+  };
+
+
   return (
     <AppLayout breadcrumbs={breadcrumbs}>
       <Head title={album.title} />
-       <AppFeatureLayout
-              title={album.title}
-              description="Viendo álbum"
-              action={{ route: route('albums.add', { id: album.id }), text: 'Añadir imagenes' }}
-            >
-            <DataTableToggle view={view} setView={setView} />
-            {view === 'grid' ? <DataGrid columns={columns} contentColumns={['image']} footerColumns={['size', 'actions']} data={album.images} filterFields={['title', 'description', 'tags']} /> : <DataTable columns={columns} data={album.images} filterFields={['title', 'description', 'tags']} visibleColumns={['select', 'title', 'filename', 'tags', 'description', 'size', 'created_at', 'actions']} />}
-        </AppFeatureLayout>
+      <AppFeatureLayout
+        title={album.title}
+        description="Viendo álbum"
+      >
+        <Accordion type='single' collapsible className='w-full pb-6 justify-center'>
+          <AccordionItem value='item-1' className='w-full text-center'>
+            <AccordionTrigger className='border-b border-gray-600 rounded-none cursor-pointer py-3'>
+              Añadir nuevas imagenes
+            </AccordionTrigger>
+            <AccordionContent>
+              <form onSubmit={handleSubmit} className="space-y-4 pb-6">
+                {/* Multiple File Upload Field */}
+                <FormField
+                  id="images"
+                  label=""
+                  inputType="custom"
+                  error={errors.images}
+                >
+                  <ImageUploader
+                    images={data.images}
+                    isUploading={isUploadingImages}
+                    onAddImages={handleAddImages}
+                    onRemoveImage={handleRemoveImage}
+                  />
+                </FormField>
+
+                {/* Submit Button */}
+                <div className="flex justify-center">
+                  <Button type="submit" disabled={processing} className="bg-blue-500 hover:bg-blue-400 min-w-xs transition-colors text-white px-4 py-6 rounded cursor-pointer">
+                    {processing && <LucideLoaderCircle className="h-4 w-4 animate-spin" />}
+                    {processing ? "Subiendo imagenes..." : "Subir imagenes"}
+                  </Button>
+                </div>
+              </form>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+
+        <DataTableToggle view={view} setView={setView} />
+        {view === 'grid' ? <DataGrid columns={columns} contentColumns={['image']} footerColumns={['size', 'actions']} data={album.images} filterFields={['title', 'description', 'tags']} /> : <DataTable columns={columns} data={album.images} filterFields={['title', 'description', 'tags']} visibleColumns={['select', 'title', 'filename', 'tags', 'description', 'size', 'created_at', 'actions']} />}
+      </AppFeatureLayout>
     </AppLayout>
   );
 }
